a5208214331fd2ed817ce40594e47b2c6cdee8fd
Tomáš Suchý <tomysuchy@seznam.cz>
2018-12-28 16:33:17 +0100

    firebase config added

---
+++ functions/src/config/firebase.js
@@ -0,0 +1,15 @@
+<script src="https://www.gstatic.com/firebasejs/5.7.0/firebase.js"></script>
+// Initialize Firebase
+<script>
+var config = {
+    apiKey: "AIzaSyDqlxiv0W1uLtvM3XrrKU75rZ3HfL7Jwb8",
+    authDomain: "pwa-bp.firebaseapp.com",
+    databaseURL: "https://pwa-bp.firebaseio.com",
+    projectId: "pwa-bp",
+    storageBucket: "pwa-bp.appspot.com",
+    messagingSenderId: "198778315233"
+};
+firebase.initializeApp(config);
+</script>
+
+export default firebase;
